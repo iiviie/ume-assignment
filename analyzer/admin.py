dac5442a405df5d42ca5bbf03bc21865d75f821d
iiviie <divyanshverma158@gmail.com>
2025-04-21 17:59:07 +0000

    make querylog model, update the admin panel

--- analyzer/admin.py
+++ analyzer/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import QueryLog
 
-# Register your models here.
+@admin.register(QueryLog)
+class QueryLogAdmin(admin.ModelAdmin):
+    list_display = ('timestamp', 'query_preview', 'tone', 'intent')
+    search_fields = ('query', 'tone', 'intent')
+    list_filter = ('tone', 'intent', 'timestamp')
+
+    def query_preview(self, obj):
+        return obj.query[:50]
+    query_preview.short_description = 'Query Preview'
+
+
